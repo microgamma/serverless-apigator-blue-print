2119034cb8fc3aca397bf25ce1d2845d35df71de
Davide Cavaliere <cavaliere.davide@gmail.com>
2020-01-22 16:22:59 +0100

    adding lambda with path param and logic to switch between aws and express.js

--- src/my-first.service.ts
+++ src/my-first.service.ts
@@ -1,10 +1,17 @@
-import { Endpoint, Lambda } from '@microgamma/apigator';
+// tslint:disable: object-literal-sort-keys no-string-literal
+import { AwsEventHandler, Endpoint, ExpressEventHandler, Lambda, LambdaDefaultHandler, Path } from '@microgamma/apigator';
+import { Injectable } from '@microgamma/digator';
 
 @Endpoint({
   cors: true,
   name: 'my-first-service',
-  private: false
+  private: false,
+  providers: [{
+    provide: LambdaDefaultHandler,
+    implementation: process.env['environment'] === 'local' ? ExpressEventHandler : AwsEventHandler
+  }]
 })
+@Injectable()
 export class MyFirstService {
 
   @Lambda({
@@ -13,5 +20,13 @@ export class MyFirstService {
   })
   public index() {
     return `Hello world! Today is ${new Date().toISOString()}`;
+  }
+
+  @Lambda({
+    method: 'GET',
+    path: process.env['environment'] === 'local' ? '/me/:name' : '/me/{name}'
+  })
+  public me(@Path('name') name) {
+    return `Hello ${name}`;
   }
 }
